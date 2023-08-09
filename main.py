cdea4909a3163e0ff9ed4ca0384554e4a5fa61a0
maher-cshub <maherarab.1299@gmail.com>
2023-08-09 23:15:13 +0100

    updated UI + sound recording

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
+
+import sounddevice
+from scipy.io.wavfile import write
 import eel
+
+
+
+
+
 
 # name of folder where the html, css, js, image files are located
 eel.init('templates')
 
 @eel.expose
 def Record(name):
-    print(name)
-    return f"recorded as {name}"
+    fs=44100 #sample_rate
+    print("Recording....\n")
+    while True:
+        record_voice=sounddevice.rec(frames=fs,channels=2)
+        sounddevice.wait()
+        result = eel.Check_Recording()()
+        print(result)
+        if (result):
+            break
+    Save(name,record_voice,fs)
+        
+
+
+
+@eel.expose
+def Save(name,audio,fs):
+        write(f"{name}.wav",fs,audio)
+        print("Finished...\nPlease Check it...")
+        return
 
 # 1000 is width of window and 600 is the height
 eel.start('index.html', size=(1000, 600))
+
+
+
--- templates/script.js
+++ templates/script.js
@@ -6,5 +6,30 @@ function setValue(res) {
 }
 
 record_btn.addEventListener("click",(ev)=>{
-    eel.Record("test")(setValue)
+    
+    let record_btn = document.getElementById("record_btn")
+    console.log(record_btn.getAttribute("status"))
+    const file_name = document.getElementById("voice_name")
+    
+    if(record_btn.getAttribute("status") == "False"){
+        eel.Record(file_name.value)(setValue)
+    }
+    else{
+        record_btn.setAttribute.status = "True"
+    }
+    
+    
 })
+
+
+eel.expose(Check_Recording);
+function Check_Recording() {
+    let record_btn = document.getElementById("record_btn")
+    if(record_btn.getAttribute("status") == "False"){
+        return false
+    }
+    else{
+        return true
+    }
+
+}
