b679b00f76d485ee961a0aa3b069b1a0f2cb4e91
maher-cshub <maherarab.1299@gmail.com>
2023-09-22 23:28:39 +0100

    bug: cant record second time

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 
-import sounddevice
+import wave
 import pyaudio
 from scipy.io.wavfile import write
 import eel
+import time
 
 
 # name of folder where the html, css, js, image files are located
@@ -12,42 +13,67 @@ eel.init('templates')
 
 class Recorder():
 
-
     def __init__(self):
-        self.rec1 = pyaudio.PyAudio()
+        self.recorder = pyaudio.PyAudio()
         self.input_devices = self.get_input_device()
+        self.mic_select = ""
+        self.file_name = ""
+        self.stream_audio = ""
+        self.audio_data = []
+        self.data = ""
 
     def get_input_device(self):
-        total_devices = self.rec1.get_host_api_count()
+        total_devices = self.recorder.get_host_api_count()
         devices = {}
         for i in range(total_devices):
-            devices[i] = self.rec1.get_device_info_by_index(i)['name']
+            devices[i] = self.recorder.get_device_info_by_index(i)['name']
         return devices
-
-
-@eel.expose
-def Record(name):
-    recorder = Recorder()
-    eel.Set_Mics(recorder.input_devices)()
-    return
-    fs=44100 #sample_rate
-    print("*********Recording****************\n")
-    while True:
-        record_voice=sounddevice.rec(frames=fs,channels=2)
-        sounddevice.wait()
-        result = eel.Check_Recording()()
-        if (result == "false"):
-            break
-    Save(name,record_voice,fs)
+    
+    def record(self,status):
         
+        if (status == "inactive"):
+            new_state = eel.Change_Status("recording")()
+            self.stream_audio = self.recorder.open(input=True,input_device_index=int(self.mic_select),rate=44000,channels=10,format=pyaudio.paInt16,frames_per_buffer=1024)
+            self.stream_audio.start_stream()
+            while new_state == "recording":
+                self.data = self.stream_audio.read(1024)
+                self.audio_data.append(self.data)
+                new_state = eel.Check_Status()()
+            
+        elif(status == "recording"):
+            if (self.stream_audio.is_active):
+                self.stream_audio.stop_stream()
+                self.stream_audio.close()
+            eel.Change_Status("inactive")()
+            
+    
+    def save(self):
+         self.recorder.terminate()
+         sound_file = wave.open(self.file_name+".wav","wb")
+         sound_file.setnchannels(10)
+         sound_file.setsampwidth(self.recorder.get_sample_size(pyaudio.paInt16))
+         sound_file.setframerate(44000)
+         sound_file.writeframes(b"".join(self.audio_data))
 
-
+rec = Recorder()
 
 @eel.expose
-def Save(name,audio,fs):
-        write(f"{name}.mp3",fs,audio)
-        print("Finished...\nPlease Check it...")
-        return
+def Start():
+    eel.Set_Mics(rec.input_devices)()
+
+@eel.expose
+def Set_Vars(file_name,mic_select):
+    rec.file_name = file_name
+    rec.mic_select = mic_select
+
+@eel.expose
+def Record(status):
+    rec.record(status)
+
+        
+@eel.expose
+def Save():
+    rec.save()
 
 @eel.expose
 def Resume(name,audio,fs):
--- templates/script.js
+++ templates/script.js
@@ -4,34 +4,41 @@ function setValue(res) {
     console.log(res)
 }
 
+
+eel.Start()
+
 document.getElementById("record_btn").addEventListener("click",(ev)=>{
-    
+
     let record_btn = document.getElementById("record_btn")
-    const file_name = document.getElementById("voice_name")
 
+    if (record_btn.getAttribute("status") == "inactive"){
+        let mic_select= document.getElementById('input_devices')
+        let file_name = document.getElementById("voice_name")
+        const chosen_mic = mic_select.options[mic_select.selectedIndex]
+        eel.Set_Vars(file_name.value,chosen_mic.getAttribute("item_id"))    
+    }
+    
+    eel.Record(record_btn.getAttribute("status"))
 
-    if(record_btn.getAttribute("state") == "False"){
-        record_btn.setAttribute("state","True")
-        eel.Record(file_name.value)(setValue)
-    }
-    else{
-        record_btn.setAttribute("state","False")
-    }
     
 })
 
 
-eel.expose(Check_Recording);
-function Check_Recording() {
-    let record_btn = document.getElementById("record_btn")
-    if(record_btn.getAttribute("state") == "False"){
-        
-        return "false"
-    }
-    else{
-        return "true"
-    }
+document.getElementById("save_btn").addEventListener("click",(evt)=>{
+    eel.Save()
+})
 
+eel.expose(Change_Status);
+function Change_Status(status) {
+    let record_btn = document.getElementById("record_btn")
+    record_btn.setAttribute("status",status)
+    return record_btn.getAttribute("status")
+}
+
+eel.expose(Check_Status);
+function Check_Status() {
+    let record_btn = document.getElementById("record_btn")
+    return record_btn.getAttribute("status")
 }
 
 eel.expose(Set_Mics);
@@ -39,7 +46,6 @@ function Set_Mics(input_devices){
     let mic_select= document.getElementById('input_devices')
     const devices = Object.entries(input_devices)
     devices.forEach(element => {
-        console.log(element)
         let new_el = document.createElement("option")
         new_el.setAttribute("item_id",element[0].toString())
         new_el.innerHTML = element[1]
