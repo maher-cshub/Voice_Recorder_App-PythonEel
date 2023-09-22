50b0766ca434b26a8c6dcce7b628c9c922a2ebbf
maher-cshub <maherarab.1299@gmail.com>
2023-09-22 23:52:39 +0100

    fix: fixed bug 'can't record second time'

--- main.py
+++ main.py
@@ -10,11 +10,10 @@ import time
 eel.init('templates')
 
 
-
 class Recorder():
 
     def __init__(self):
-        self.recorder = pyaudio.PyAudio()
+        self.recorder = ""
         self.input_devices = self.get_input_device()
         self.mic_select = ""
         self.file_name = ""
@@ -23,29 +22,38 @@ class Recorder():
         self.data = ""
 
     def get_input_device(self):
-        total_devices = self.recorder.get_host_api_count()
+        p_audio = pyaudio.PyAudio()
+        total_devices = p_audio.get_host_api_count()
         devices = {}
         for i in range(total_devices):
-            devices[i] = self.recorder.get_device_info_by_index(i)['name']
+            devices[i] = p_audio.get_device_info_by_index(i)['name']
+        p_audio.terminate()
         return devices
     
-    def record(self,status):
-        
+    def record(self,status):      
         if (status == "inactive"):
-            new_state = eel.Change_Status("recording")()
-            self.stream_audio = self.recorder.open(input=True,input_device_index=int(self.mic_select),rate=44000,channels=10,format=pyaudio.paInt16,frames_per_buffer=1024)
-            self.stream_audio.start_stream()
-            while new_state == "recording":
-                self.data = self.stream_audio.read(1024)
-                self.audio_data.append(self.data)
-                new_state = eel.Check_Status()()
+            self.recorder = pyaudio.PyAudio()
+            self.listen()
             
         elif(status == "recording"):
-            if (self.stream_audio.is_active):
-                self.stream_audio.stop_stream()
-                self.stream_audio.close()
-            eel.Change_Status("inactive")()
+            self.pause()
+
+        elif(status == "paused"):
+            self.listen()
+
+    def listen(self):
+        new_state = eel.Change_Status("recording")()
+        self.stream_audio = self.recorder.open(input=True,input_device_index=int(self.mic_select),rate=44000,channels=10,format=pyaudio.paInt16,frames_per_buffer=1024)
+        while new_state == "recording":
+            self.data = self.stream_audio.read(1024)
+            self.audio_data.append(self.data)
+            new_state = eel.Check_Status()()
             
+    def pause(self):
+        eel.Change_Status("paused")()
+        self.stream_audio.stop_stream()
+        self.stream_audio.close()
+        
     
     def save(self):
          self.recorder.terminate()
@@ -54,6 +62,7 @@ class Recorder():
          sound_file.setsampwidth(self.recorder.get_sample_size(pyaudio.paInt16))
          sound_file.setframerate(44000)
          sound_file.writeframes(b"".join(self.audio_data))
+         eel.Change_Status("inactive")()
 
 rec = Recorder()
 
@@ -69,16 +78,10 @@ def Set_Vars(file_name,mic_select):
 @eel.expose
 def Record(status):
     rec.record(status)
-
         
 @eel.expose
 def Save():
     rec.save()
-
-@eel.expose
-def Resume(name,audio,fs):
-     return
-     
 
 # 1000 is width of window and 600 is the height
 eel.start('index.html', size=(1000, 600))
